efb67d3c109cc9b22be3fed3f60f096a3cf333f4
Rodrigo Santos <rodrigom376@gmail.com>
2023-08-27 15:30:38 -0300

    init

---
+++ src/SimuladorContaBancaria.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+class ContaBancaria {
+    private double saldo;
+
+    public ContaBancaria(double saldoInicial) {
+        saldo = saldoInicial;
+    }
+
+    public double consultarSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Depósito de " + valor + " realizado com sucesso.");
+    }
+
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Saque de " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente para saque.");
+        }
+    }
+}
+
+public class SimuladorContaBancaria {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ContaBancaria conta = new ContaBancaria(1000.0); // Saldo inicial de R$ 1000.00
+
+        while (true) {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1 - Consultar saldo");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("4 - Sair");
+            int escolha = scanner.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    System.out.println("Saldo atual: R$" + conta.consultarSaldo());
+                    break;
+                case 2:
+                    System.out.print("Digite o valor para depositar: ");
+                    double valorDeposito = scanner.nextDouble();
+                    conta.depositar(valorDeposito);
+                    break;
+                case 3:
+                    System.out.print("Digite o valor para sacar: ");
+                    double valorSaque = scanner.nextDouble();
+                    conta.sacar(valorSaque);
+                    break;
+                case 4:
+                    System.out.println("Encerrando o programa.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Opção inválida. Escolha novamente.");
+            }
+        }
+    }
+}
